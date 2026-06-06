6fed0cebe2513c736620eb81ea34971fe36345c8
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-06-06 14:00:17 +0530

    Create a basic Hello world program

---
+++ Basic/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        System.out.println("Welcome to the world of java programming.");
+        
+        System.out.println("lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\ntempor incididunt ut labore et dolore magna aliqua.");
+
+    }
+}
