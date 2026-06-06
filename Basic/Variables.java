b57eca8c2967a7ab621b43e72a6d0407fe2a33e5
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-06-06 20:41:05 +0530

    Variables in java

---
+++ Basic/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        int x = 5;
+        System.out.println(x);
+
+        int y; //intialization
+        y = 20; //declarations
+        System.out.println(y);
+        
+        int z = x * y;
+        System.out.println(z);
+
+        x = 10;
+        System.out.println(x);
+
+    }
+}
