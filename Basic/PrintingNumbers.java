2cbbbbd0cc06445399788277e091b2cd6b759e44
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-06-06 20:05:50 +0530

    printing numbers

---
+++ Basic/PrintingNumbers.java
@@ -0,0 +1,8 @@
+public class PrintingNumbers {
+    public static void main(String[] args) {
+
+        System.out.println(69+1); //This is the integer 
+        System.out.println("69 + 1"); //This is the String 
+        
+    }
+}
